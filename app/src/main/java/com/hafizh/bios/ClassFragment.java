fa577a7013a53b86c3b51c994d15b4f5176acb6a
hafizh.project45 <hafizh.project45@gmail.com>
2022-11-27 10:15:41 +0700

    version 1.0

---
+++ app/src/main/java/com/hafizh/bios/ClassFragment.java
@@ -0,0 +1,42 @@
+package com.hafizh.bios;
+
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ClassFragment extends Fragment {
+
+//    Context context;
+//    RecyclerView recyclerView;
+//    RecyclerView.Adapter recycleViewAdapter;
+//    RecyclerView.LayoutManager recycleViewLayoutManager;
+//
+//    String[] subjects1 = {
+//            "Rekayasa Perangkat Lunak dan Gim 1","Rekayasa Perangkat Lunak dan Gim 2","Desain Komunikasi Visual 1","Desain Komunikasi Visual 2",
+//            "Teknik Jaringan Komputer dan Telekomunikasi 1","Teknik Jaringan Komputer dan Telekomunikasi 2"
+//    };
+//    String[] subjects2 = {
+//            "Rekayasa Perangkat Lunak dan Gim 1","Rekayasa Perangkat Lunak dan Gim 2","Desain Komunikasi Visual 1","Desain Komunikasi Visual 2",
+//            "Teknik Jaringan Komputer dan Telekomunikasi 1","Teknik Jaringan Komputer dan Telekomunikasi 2"
+//    };
+//    String[] subjects3 = {
+//            "Rekayasa Perangkat Lunak dan Gim 1","Rekayasa Perangkat Lunak dan Gim 2","Desain Komunikasi Visual 1","Desain Komunikasi Visual 2",
+//            "Teknik Jaringan Komputer dan Telekomunikasi 1","Teknik Jaringan Komputer dan Telekomunikasi 2"};
+
+
+
+    public ClassFragment() {
+
+//        context = context.getApplicationContext();
+//        recyclerView = recyclerView.findViewById(R.id.recyclerView);
+//        recycleViewLayoutManager = new LinearLayoutManager(context);
+//        recyclerView.setLayoutManager(recycleViewLayoutManager);
+//        recycleViewAdapter = new AdapterRecycleView(context, subjects1);
+//        recyclerView.setAdapter(recycleViewAdapter);
+//
+//
+
+        };
+    }
---
+++ app/src/main/java/com/hafizh/bios/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.hafizh.bios;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(decorView.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
